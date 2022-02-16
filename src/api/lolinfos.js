a0f7b7f9a87f6fa43e528be17843891952009581
JongCHONG <yonhoa27@hotmail.fr>
2022-02-16 19:02:16 +0100

    dotenv

--- src/api/lolinfos.js
+++ src/api/lolinfos.js
@@ -1,4 +1,4 @@
-const api_key = "RGAPI-8cc72b5e-6e5c-47f6-b8a3-e012a2a92150"
+const api_key = process.env.REACT_APP_API_KEY
 
 const getLolProfile = async (summoner_name) => {
   const LoL_Response = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner_name}?api_key=${api_key}`, {
